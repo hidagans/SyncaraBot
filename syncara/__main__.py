fd7766ebf45771007613fdc239507a82c3767d7e
hidagans <58388773+hidagans@users.noreply.github.com>
2025-07-01 12:58:21 +0700

    Update __main__.py

--- syncara/__main__.py
+++ syncara/__main__.py
@@ -6,6 +6,8 @@ from pyrogram import idle
 from importlib import import_module
 from syncara import *
 from syncara.modules import loadModule
+from syncara.userbot import initialize_userbots, stop_userbots
+from config.config import USERBOTS
 
 shutdown_event = asyncio.Event()  # Event untuk menangani shutdown
 
