75af64013de7afdc6461147c14580e588253a311
hidagans <hidahida748@gmail.com>
2025-07-02 20:54:18 +0700

    anjay

--- config/config.py
+++ config/config.py
@@ -14,12 +14,12 @@ OWNER_ID = [
 ]
 
 # Userbot Configuration
-USERBOTS = [
+[
     {
-        "name": "userbot1",
+        "name": "userbott1",
         "api_id": 21445722,
         "api_hash": "710f18f90849255dd85837d00d5fe85f",
-        "session_string": "BQFHPFoASBpAM264M_qZNLbKwTlIpWBy7zLZymCG5qj_G5X0O_8dUhhyskwucAZ-o--_ChHY-Nm7c3ckczZQGQpye_6i49vjWq6y7cTOZK1Czr4hwUnc7k41fmpvEpqizOxGCsJpcLUsD2Rxa27MJRfhrndKx-7GTdoBBi2FbyELu20iieoA15w9Sb6CH5oq8aREYeuziB37f-Eid2Vfu464J2Q_-7qsluhvzSynrFB0aTvOeC0Tm482E_V2D4uneDRHrWkDMb1rhwCugsSO53hHVKtFXYg25nDeUfTYxrEBy5Z0Ft-ZmSuDvPupEXITos3MqcPh06IkJG0u7Ex6wr4rvafT0QAAAAGc3e6SAA"
+        "session_string": "BQFHPFoAoAqzuRBOGZOnI-4Dt5nyBY2FZyZGn4MDxyOAXdec0AF93QsetfmBPeM8lOPveH_GAXIhXI-OeIfCi0Df5YY3ldDpaxzfgjf81bd_PUhbHZ7JJaCZs3T8k-sWAYEQyT4K9MPSO3ScNw22fKRao6YWkWhhb27ZUGBmuNqV1HkY9xqc_FSK9uKSwj9Yhkw882OGPhN7kuMiWXpYUorB6y9ofcEBTM52ZO6i3EtDQ7-opmwOx8WtRF8YsL3sB84LErEOWbCR8fDSe20icc-WkgDd4JMU_I-5sbbBlyFqOtm0SKUvdJ05rTfeg4-KHUco5pFvnHEEP_4FAumdML3QMDqp2wAAAAGc3e6SAA"
     }
 ]
 
