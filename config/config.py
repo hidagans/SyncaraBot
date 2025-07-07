968483062615702ab46619e47eaad8c902cf2bb7
hidagans <hidahida748@gmail.com>
2025-07-07 08:13:33 +0700

    anjay

--- config/config.py
+++ config/config.py
@@ -13,8 +13,6 @@ OWNER_ID = [
     6696975845
 ]
 
-SESSION_STRING=os.getenv("SESSION_STRING")
-
 # MongoDB configuration
 MONGO_URI = os.getenv("MONGO_URI")
 DB_NAME = os.getenv("DB_NAME", "syncara_bot")
